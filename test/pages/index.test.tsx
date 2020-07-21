6cbb45e3dc9cdd5ccad35b2903477ecf58efa177
Davy Hausser <davy.hausser@gmail.com>
2020-07-22 00:51:01 +0200

    :recycle: client data fetching only

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -2,39 +2,40 @@ import React from 'react'
 import { render } from '../testUtils'
 import { Home } from '../../pages/index'
 
-const posts = [
-  {
-    id: 1,
-    title: 'Hello World',
-    content: 'This is the first hello world content',
-    author: {
-      email: 'alice@prisma.io',
-    },
-    published: false,
-  },
-  {
-    id: 2,
-    title: 'Hello Second World',
-    content: 'This is the second hello world content',
-    author: {
-      email: 'mark@prisma.io',
-    },
-    published: false,
-  },
-  {
-    id: 3,
-    title: 'Hello Third World',
-    content: 'This is the third hello world content',
-    author: {
-      email: 'rob@prisma.io',
-    },
-    published: true,
-  },
-]
+// const posts = [
+//   {
+//     id: 1,
+//     title: 'Hello World',
+//     content: 'This is the first hello world content',
+//     author: {
+//       email: 'alice@prisma.io',
+//     },
+//     published: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Hello Second World',
+//     content: 'This is the second hello world content',
+//     author: {
+//       email: 'mark@prisma.io',
+//     },
+//     published: false,
+//   },
+//   {
+//     id: 3,
+//     title: 'Hello Third World',
+//     content: 'This is the third hello world content',
+//     author: {
+//       email: 'rob@prisma.io',
+//     },
+//     published: true,
+//   },
+// ]
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home posts={posts} />, {})
+    // const { asFragment } = render(<Home posts={posts} />, {})
+    const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 })
--- test/pages/post.test.tsx
+++ test/pages/post.test.tsx
@@ -2,19 +2,20 @@ import React from 'react'
 import { render } from '../testUtils'
 import { Post } from '../../pages/posts/[id]'
 
-const post = {
-  id: 1,
-  title: 'Hello World',
-  content: 'This is the hello world content',
-  author: {
-    email: 'alice@prisma.io',
-  },
-  published: false,
-}
+// const post = {
+//   id: 1,
+//   title: 'Hello World',
+//   content: 'This is the hello world content',
+//   author: {
+//     email: 'alice@prisma.io',
+//   },
+//   published: false,
+// }
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Post post={post} />, {})
+    // const { asFragment } = render(<Post post={post} />, {})
+    const { asFragment } = render(<Post />, {})
     expect(asFragment()).toMatchSnapshot()
   })
 })
