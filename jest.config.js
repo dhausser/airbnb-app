5632e205bab24d2da5ef10f7af71e90ef1c01fa0
Davy Hausser <davy.hausser@gmail.com>
2020-07-30 18:37:40 +0200

    ✅ apollo mocked provider abstracted out to test-utils render wrapper

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   roots: ['<rootDir>'],
   moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
-  // setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
   transform: {
--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render as defaultRender, cleanup, waitFor } from '@testing-library/react'
+import { render as defaultRender, cleanup, waitFor, RenderResult } from '@testing-library/react'
 import { RouterContext } from 'next/dist/next-server/lib/router-context'
 import { NextRouter } from 'next/router'
 import { MockedProvider, MockedResponse } from '@apollo/client/testing'
@@ -27,13 +27,13 @@ type RenderOptions = DefaultParams[1] & {
 
 export function render(
   ui: RenderUI,
-  { wrapper, router, mocks, cache, addTypename, ...options }: RenderOptions = {}
-) {
+  { wrapper, router, mocks, cache, addTypename, ...options }: RenderOptions
+): RenderResult {
   if (!wrapper) {
     wrapper = ({ children }) => (
       <RouterContext.Provider value={{ ...mockRouter, ...router }}>
         <MockedProvider mocks={mocks} cache={cache} addTypename={addTypename}>
-          {children}
+          {children as React.ReactElement}
         </MockedProvider>
       </RouterContext.Provider>
     )
@@ -63,110 +63,3 @@ const mockRouter: NextRouter = {
   },
   isFallback: false,
 }
-
-// import { render } from '@testing-library/react'
-// import { RouterContext } from "next/dist/next-server/lib/router-context";
-// // import { MockedProvider } from '@apollo/client/testing'
-// // import { ThemeProvider } from "my-ui-lib"
-// // import { TranslationProvider } from "my-i18n-lib"
-// // import defaultStrings from "i18n/en-x-default"
-
-// // Mocks useRouter
-// const useRouter = jest.spyOn(require('next/router'), 'useRouter')
-
-// /**
-//  * mockNextUseRouter
-//  * Mocks the useRouter React hook from Next.js on a test-case by test-case basis
-//  */
-// export function mockNextUseRouter(props: {
-//   route: string
-//   pathname: string
-//   query: string
-//   asPath: string
-// }): void {
-//   useRouter.mockImplementationOnce(() => ({
-//     route: props.route,
-//     pathname: props.pathname,
-//     query: props.query,
-//     asPath: props.asPath,
-//   }))
-// }
-
-// const Providers = ({ children }) => {
-//   return children
-//   // return <MockedProvider mocks={}>{children}</MockedProvider>
-//   // return (
-//   //   <ThemeProvider theme="light">
-//   //     <TranslationProvider messages={defaultStrings}>
-//   //       {children}
-//   //     </TranslationProvider>
-//   //   </ThemeProvider>
-//   // )
-// }
-
-// const customRender = (ui: JSX.Element, options = {}): unknown =>
-//   render(ui, { wrapper: Providers, ...options })
-
-// // re-export everything
-// export * from '@testing-library/react'
-
-// // override render method
-// export { customRender as render }
-
-// import { render } from '@testing-library/react'
-
-// // this adds custom jest matchers from jest-dom
-// import { MockedProvider, MockedResponse } from '@apollo/client/testing'
-// import { ApolloCache } from '@apollo/client'
-
-// type RenderApolloOptions = {
-//   mocks?: MockedResponse[]
-//   addTypename?: boolean
-//   defaultOptions?: any
-//   cache?: ApolloCache<{}> | undefined
-//   resolvers?: any
-//   wrapper?: any
-//   [st: string]: any
-// }
-
-// const renderApollo = (
-//   node: JSX.Element,
-//   { mocks, addTypename, defaultOptions, cache, resolvers, ...options }: RenderApolloOptions = {}
-// ): unknown => {
-//   return render(
-//     <MockedProvider
-//       mocks={mocks}
-//       addTypename={addTypename}
-//       defaultOptions={defaultOptions}
-//       cache={cache}
-//       resolvers={resolvers}
-//     >
-//       {node}
-//     </MockedProvider>,
-//     options
-//   )
-// }
-
-// // Mocks useRouter
-// const useRouter = jest.spyOn(require('next/router'), 'useRouter')
-
-// /**
-//  * mockNextUseRouter
-//  * Mocks the useRouter React hook from Next.js on a test-case by test-case basis
-//  */
-// export function mockNextUseRouter(props: {
-//   route: string
-//   pathname: string
-//   query: string
-//   asPath: string
-// }): void {
-//   useRouter.mockImplementationOnce(() => ({
-//     route: props.route,
-//     pathname: props.pathname,
-//     query: props.query,
-//     asPath: props.asPath,
-//   }))
-// }
-
-// export * from '@testing-library/react'
-// export { renderApollo }
