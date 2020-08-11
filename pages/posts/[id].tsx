d4c5b2a229acdf8f6ce3ebca1a7169909b748b6e
Davy Hausser <davy.hausser@gmail.com>
2020-08-11 17:11:05 +0200

    :sparkles: frontend integration of update and delete post started

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,15 +4,28 @@ import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
 
 import { initializeApollo } from '../../apollo/client'
 import { GET_POST_QUERY } from '../../apollo/queries'
-import { usePost } from '../../apollo/hooks'
+import { usePost, useUpdatePost, useDeletePost } from '../../apollo/hooks'
 import { PostCard } from '../../components/post-card'
 
 import styles from '../../styles/Home.module.css'
 
+/**
+ * TODO: Update post with input variables
+ * TODO: Update post cache update and optimistic response
+ * TODO: Delete post cache update and optimistic response
+ * TODO: Delete post redirect to homepage on succes
+ */
 export const SinglePost: React.FC = () => {
   const router: NextRouter = useRouter()
   const { id } = router.query
   const { loading, error, data } = usePost(id)
+  const [updatePost] = useUpdatePost({
+    id: data?.post.id as string,
+    title: data?.post.title as string,
+    content: data?.post.content,
+    authorEmail: data?.post.author?.email as string,
+  })
+  const [deletePost] = useDeletePost({ id: data?.post.id as string })
 
   if (error) return <p>{`${error.name}: ${error.message}`}</p>
   if (loading || !data) return <p>Loading...</p>
@@ -22,6 +35,8 @@ export const SinglePost: React.FC = () => {
       <div className={styles.main}>
         <div className={styles.grid}>
           <PostCard post={data.post} />
+          <button onClick={() => updatePost()}>Update</button>
+          <button onClick={() => deletePost()}>Delete</button>
         </div>
       </div>
     </div>
